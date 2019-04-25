bd58e7b7b0f0a5a5446f65ecb2ace2682c08eb8c
sagar <pandyasagar323@gmail.com>
2019-04-24 23:50:49 -0400

    HOTEL WEBSITE

--- Hotel/admin/db.php
+++ Hotel/admin/db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","password","finalhotel",3306) or die(mysql_error());
+$con = mysqli_connect("localhost","root","SAGU","team_c_db_test",3306) or die(mysql_error());
 
 ?>
